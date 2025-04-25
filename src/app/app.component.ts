65ad663c2f4f4d9d8e409177110e37f365bd097f
Dennis Dumin <164201997+DennisDumin@users.noreply.github.com>
2025-04-25 13:23:50 +0200

    deleted about me in appcomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { CommonModule } from '@angular/common';
-import { AboutMeComponent } from "./components/about-me/about-me.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, CommonModule, HeaderComponent, AboutMeComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, CommonModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
